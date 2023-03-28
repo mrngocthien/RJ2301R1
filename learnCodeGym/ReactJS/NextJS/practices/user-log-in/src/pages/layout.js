56883a6f8e77654473c095f16c241e9bf0428b98
mrngocthien <108567256+mrngocthien@users.noreply.github.com>
2023-03-28 07:22:58 +0700

    s

---
+++ learnCodeGym/ReactJS/NextJS/practices/user-log-in/src/pages/layout.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from '../styles/layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/components/about">
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link href="/components/blog">
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link href="/components/login">
+            Logout
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.container}>{children}</div>
+    </div>
+  );
+}
